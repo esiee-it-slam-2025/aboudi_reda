43d4339cfcae68b056d92e0540379b757fc2b906
Reda951 <144052852+Reda951@users.noreply.github.com>
2024-02-19 16:19:07 +0100

    commit`

---
+++ exo1.py
@@ -0,0 +1,24 @@
+# Copiez ce fichier dans votre repo PERSONNEL
+# Tapez votre code ci dessous
+nombrea = 12.34
+
+nombreb = int(input("veuillez saisir votre nombreb !"))
+
+def somme(nombrea, nombreb):
+    return (nombrea + nombreb) * 2
+total = somme(nombrea, nombreb)
+print(total) 
+if total > 20:
+    print("Résultat énorme")
+else : 
+    print ("Résultat pas ouf")
+
+
+
+
+
+
+
+
+
+# puis executer ce fichier dans un terminal avec la commande "py ex_intro.py"
