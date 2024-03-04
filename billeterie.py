913f1de6df70beb8821c7df50c5dd2121789087d
Reda951 <redaaliaboudi@gmail.com>
2024-03-04 16:07:01 +0100

    dsdsd

--- billeterie.py
+++ billeterie.py
@@ -109,5 +109,5 @@ if depart_index < arrivee_index:
 elif depart_index > arrivee_index:  
     print("\nVeuillez emprunter la voie 2.")
 else:
-    print("\nVous êtes déjà à destination.")
+    print("\nmdr bah t'es déja dans ta gare.")
 
