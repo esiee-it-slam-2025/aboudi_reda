623ac03f0c719b12b59e2bc8abcd9bfba2f7f4d0
Reda951 <redaaliaboudi@gmail.com>
2024-04-15 14:58:28 +0200

    poo2

--- TP POO/poo.py
+++ TP POO/poo.py
@@ -1,55 +1,91 @@
-# Copiez ce fichier dans votre repo PERSONNEL
-# Tapez votre code ci dessous
-# puis executer ce fichier dans un terminal avec la commande "py entrainement_poo.py"
+import random
 
-# Init variables
-hero = None
-monstre = None
+class Creature:
+    def __init__(self, nom, pv, degats, etat=None):
+        self.nom = nom
+        self.pv = pv
+        self.degats = degats
+        self.etat = etat
 
-armes = {"Epée": 5, "Arc": 4, "Couteau à beurre": 1}
+    def rafale(self, cible):
+        cible.pv -= self.degats
+        print(f"{self.nom} attaque {cible.nom}.")
 
+    def bombardement(self, cible):
+        cible.etat = "explosé"
+        print(f"{self.nom} utilise un bombardement sur {cible.nom}.")
+
+class Hero(Creature):
+    def __init__(self, nom, degats, pm):
+        super().__init__(nom, 100, degats)
+        self.pm = pm
+
+    def missile(self, cible):
+        if self.pm >= 1:
+            cible.pv -= self.degats * 2
+            self.pm -= 1
+            print(f"{self.nom} lance un missile sur {cible.nom}.")
+        else:
+            print("Pas assez de roquets.")
 
 # Init classes
-
-
-class Creature():
-    # Ajouter les autres attributs
-    degats = 0
-
-    def __init__(self, degats):
-        self.degats = degats
-
-    def attaque(self, cible):
-        # Ici les concéquences de l'attaque
-        pass  # A supprimer quand la fonction sera écrite
-
-# Ajouter les autres classes
+armes = {
+    1: {"nom": "ERYX", "degats": random.randint(9, 15)},
+    2: {"nom": "SCAR-H PR", "degats": random.randint(5, 8)},
+    3: {"nom": "MP-5", "degats": random.randint(12, 21)},
+    4: {"nom": "AK-47", "degats": random.randint(22, 30)}
+}
 
 # Début du jeu
-
-
 def intro():
     print("===============================================================")
-    print("|| Bienvenue dans le système de combat POO (Punch Out Out) ! ||")
+    print("|| Bienvenue dans le système de combat POO (ARMÉE DE TERRE FRANÇAISE) ! ||")
     print("===============================================================")
-    print("\nQuel est le nom de ton héros.")
-    # Choix du nom, choix de l'arme
+    nom_hero = input("\nChoisissez le nom de votre militaire: ")
+    print("Choisissez votre arme:")
+    for numero, arme in armes.items():
+        print(f"{numero}. {arme['nom']}")
+    arme_choisie = input("Votre choix (entrez le numéro de l'arme): ")
+    while not arme_choisie.isdigit() or int(arme_choisie) not in armes:
+        print("Arme invalide.")
+        arme_choisie = input("Choisissez votre arme (entrez le numéro de l'arme): ")
 
-    # Création des instances
+    # Récupération des informations sur l'arme choisie par ton joueur
+    arme_infos = armes[int(arme_choisie)]
+    # Création du militaire avec le prénom, les dégâts de l'arme choisie et 100 pv
+    hero = Hero(nom_hero, arme_infos["degats"], 5)
+    # Mise en place des usa avec une vie aléatoires entre 60 et 150
+    monstre = Creature("USA", random.randint(60, 110), 15)
 
-    # Attention pour modifier les variables globales dans une fonction en python vous devez d'abord utiliser cette ligne
-    global heros, monstre
+    combat(hero, monstre)
 
-    combat()
+# Boucle de la guerre
+def combat(hero, monstre):
+    tour_hero = True
+    print("La guerre commence!")
 
-# Boucle de combat
+    while hero.pv > 0 and monstre.pv > 0:
+        if tour_hero:
+            print("\nTour des Français:")
+            action = input("Choisissez votre action (rafale / missile): ")
+            while action not in ["rafale", "missile"]:  
+                print("Action invalide.")
+                action = input("Choisissez votre action (rafale / missile): ")
+                
+            if action == "rafale":
+                hero.rafale(monstre)
+            elif action == "missile":
+                hero.missile(monstre)
+        else:
+            print("\nTour des USA :")
+            monstre.rafale(hero)
 
+        tour_hero = not tour_hero
 
-def combat():
-    tourHeros = True
-    print("Bagarre")
-    # Créer la boucle de combat
-
+    if hero.pv <= 0:
+        print("La France a été vaincue. Les USA gagne!")
+    else:
+        print("Les USA ont été vaincus. La France gagne!")
 
 # Pour lancer le jeu, on appelle la fonction d'introduction
 if __name__ == "__main__":
